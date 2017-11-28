53cd7c1b9e86477aa465d36cf2c6638b03c0b02f
tomhostyn <tom.hostyn@gmail.com>
2017-11-28 17:52:46 +0100

    first full version - not yet integrated

--- index.js
+++ index.js
@@ -3,26 +3,65 @@ var https = require('https')
 var xml2js = require('xml2js')
 
 
+function parseParkingBxl(xml){
+	
+	var parseString = require('xml2js').parseString;
+	let parkingRecords;
+	var parkingDict = {}
+	parseString(xml, function (err, result) {
+		parkingRecords=result.d2LogicalModel.payloadPublication[0].genericPublicationExtension[0].parkingStatusPublication[0].parkingRecordStatus;
+		
+		console.log("parseParkingBxl")
+
+		for (var i in parkingRecords) {
+			pr = parkingRecords[i]
+			console.log(pr.parkingRecordReference[0].$.id 
+				+ " " + pr.parkingSiteOpeningStatus[0] 
+				+ " " + pr.parkingSiteStatus[0])
+			
+			parkingDict[pr.parkingRecordReference[0].$.id] = {
+				id: pr.parkingRecordReference[0].$.id,
+				status: pr.parkingSiteStatus[0], 
+				open: pr.parkingSiteOpeningStatus[0]
+				}
+			}
+	   });
+	return parkingDict
+	}
+
+// from https://www.brussels-parking-guidance.com/API/API/Datex/Export?publication=static&publication=static
+var ParkingNames = {
+	"AD7B77E6-A7AA-441A-8686-7B88CE92CF96" : {
+		name: "Dansaert",
+		id:"AD7B77E6-A7AA-441A-8686-7B88CE92CF96",
+		places: "221"
+	},
+	"C4E6F4CD-BEB7-408C-A184-C68A4EFA7F5A" : {
+		name: "Louise",
+		id:"C4E6F4CD-BEB7-408C-A184-C68A4EFA7F5A",
+		places: "320"
+	},
+	"B2FAAD2C-D797-40AC-8F5B-45FDE656A257": {
+		name: "Simonis",
+		id:"B2FAAD2C-D797-40AC-8F5B-45FDE656A257",
+		places: "132"
+	}
+}
+
 /**
  * HTTP Cloud Function.
  *
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
  */
- 
-exports.helloGET = function helloGET2 (req, res) {
-	res.send('Hello World!');
-}
- 
+  
 exports.helloGET = function helloGET (req, response) {
 
 	console.log(">> helloGet")
 	console.log("v16")
   
-  // https://www.brussels-parking-guidance.com/API/API/Datex/Export?publication=dynamic&amp;publication=dynamic&amp;publication=dynamic&amp;publication=dynamic
 	var options = {
 		host: 'www.brussels-parking-guidance.com',
-//		port: 80,
 		path: '/API/API/Datex/Export?publication=dynamic&publication=dynamic'
 	};
 
@@ -41,31 +80,25 @@ exports.helloGET = function helloGET (req, response) {
 	//		console.log(body);
 			console.log("------------------------------------");
 			
-			console.log("test sample xml");
-			var parseString = xml2js.parseString;
-			var xml = "<root>Hello xml2js!</root>"
-			parseString(xml, function (err, result) {
-				console.log("parsed xml sample");
-				console.dir(result);
-				console.log(result.root)
-				console.log("parseString err")
-				console.dir(err);
-
-			});
 
 			console.log("test bxl xml");
-			var parseString = require('xml2js').parseString;
-			var xml = body
-			parseString(xml, function (err, result) {
-				console.log("parsed xml bxl");
-				console.log("in parseString")
-				console.log(result.d2LogicalModel.$.modelBaseVersion)
-
-				console.dir(result);
-				console.log("parseString err")
-				console.dir(err);
-			});
 			
+			
+			var parkingStatus = parseParkingBxl(body)
+			
+			let available = 0
+			body = ""
+			for (var i in ParkingNames){
+				if ((parkingStatus[ParkingNames[i].id].open == "open") 
+						&& (parkingStatus[ParkingNames[i].id].status == "spacesAvailable")){
+					body = ParkingNames[i].name + " has space available"
+					available += 1
+					}
+			}
+			
+			if (available == 0){
+				body = "no places available"
+			}
 
 			response.send(body);
 

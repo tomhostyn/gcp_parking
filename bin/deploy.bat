ab8970c65286b788765862224dd133969995d174
tomhostyn <tom.hostyn@gmail.com>
2017-12-03 11:16:23 +0100

    ignore node modules

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -1,6 +1,6 @@
-# 
-# GCP cloud function deploy script
-#
+rem 
+rem  GCP cloud function deploy script
+rem 
 
 gcloud beta functions deploy findParking --stage-bucket tom_hostyn_parking_staging --trigger-http
 gcloud beta functions logs read

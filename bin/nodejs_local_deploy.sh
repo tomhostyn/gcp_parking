cc93f84a8d6243d349d552a51aa797989038a4aa
tomhostyn <tom.hostyn@gmail.com>
2017-12-03 10:22:18 +0100

    move scripts to bin dir

---
+++ bin/nodejs_local_deploy.sh
@@ -0,0 +1,36 @@
+# local install setup
+
+# install bash 
+# https://www.howtogeek.com/249966/how-to-install-and-use-the-linux-bash-shell-on-windows-10/
+
+# install google cloud emulator
+#https://github.com/GoogleCloudPlatform/cloud-functions-emulator
+
+# install node in bash
+# https://aigeec.com/installing-node-js-on-windows-10-bash/
+
+PROJECT_LOCAL_DIR=/mnt/c/Users/tomho/gcp/gcp_parking/
+GCP_PROJECT_NAME="parkingfinder-6bbaa"
+
+if [ ! -d "$PROJECT_LOCAL_DIR" ]; then
+	echo "$PROJECT_LOCAL_DIR does not exist. exiting."
+	exit
+fi
+
+cd "$PROJECT_LOCAL_DIR"
+
+npm install -d
+
+echo "project name : "
+echo $GCP_PROJECT_NAME
+
+NPM_BIN=`npm bin`
+echo $GCP_PROJECT_NAME | $NPM_BIN/functions start
+
+$NPM_BIN/functions deploy helloWorld --trigger-http
+$NPM_BIN/functions deploy findParking --trigger-http
+
+
+$NPM_BIN/functions config list --json
+
+echo "access console using 'functions logs read'"
